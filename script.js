04e9c1ed4b9eac845b5d33f8df91742f6914c4d6
Bazif Abdullah <bazifabdullah@gmail.com>
2024-03-25 16:48:47 +0500

    minor adjustment

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ resetBtn.onclick = () => {
     minutes = 0;
     seconds = 0;
     miliseconds = 0;
-    appendMilisec.innerHTML = '0' + miliseconds;
-    appendSec.innerHTML = '0' + seconds;
-    appendMin.innerHTML = '0' + minutes;
+    appendMilisec.innerHTML = `0${miliseconds}`;
+    appendSec.innerHTML = `0${seconds}`;
+    appendMin.innerHTML = `0${minutes}`;
     startBtn.innerHTML = 'Start'
 }
 
@@ -36,7 +36,7 @@ function startTimer() {
     miliseconds++;
 
     if (miliseconds <= 9) {
-        appendMilisec.innerHTML = "0" + miliseconds;
+        appendMilisec.innerHTML = `0${miliseconds}`;
     }
     if (miliseconds > 9) {
         appendMilisec.innerHTML = miliseconds;
@@ -44,7 +44,7 @@ function startTimer() {
     if (miliseconds === 99) {
         miliseconds = 0;
         seconds += 1;
-        appendSec.innerHTML = '0' + seconds;
+        appendSec.innerHTML = `0${seconds}`;
     }
     if (seconds > 9) {
         appendSec.innerHTML = seconds;
@@ -52,7 +52,7 @@ function startTimer() {
     if (seconds === 60) {
         seconds = 0;
         minutes += 1;
-        appendMin.innerHTML = "0" + minutes
+        appendMin.innerHTML = `0${minutes}`;
     }
     if (minutes > 9) {
         appendMin.innerHTML = minutes;
